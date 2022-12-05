cd26f006de7eecb878f23b2e81b3175343a5c648
tunguyen <tuantu@bacoor.co>
2022-12-05 15:36:09 +0700

    main

--- services/store-bridge-info.ts
+++ services/store-bridge-info.ts
@@ -1,25 +1,62 @@
-import { BridgeInfo } from "../models";
 import { BigNumber, BigNumberish } from "ethers";
+import axios from "axios";
 
 export const storeBridgeInfo = async (
-    commitment: string,
-    token: string,
-    fee: BigNumberish,
-    value: BigNumberish,
-    relayer: string
+  commitment: string,
+  token: string,
+  fee: BigNumberish,
+  value: BigNumberish,
+  relayer: string
 ) => {
-    const bridge = new BridgeInfo({})
-    const record = {
-        commitment: commitment,
-        token: token,
-        fee: fee,
-        value: value,
-        relayer: relayer,
-    };
+  // const bridge = new BridgeInfo({});
+  const record = {
+    commitment: commitment,
+    token: token.toLowerCase(),
+    fee: fee.toString(),
+    value: value.toString(),
+    relayer: relayer,
+  };
 
-    Object.keys(record).forEach(
-        (value) => (value = BigNumber.from(value).toString())
-    );
+  // const { data: bridgeInfos } = await axios({
+  //   method: "get",
+  //   url: "https://nft-card.w3w.app/api/bridge",
+  // });
+  // console.log(bridgeInfos);
+  // return bridgeInfos;
 
-    await BridgeInfo.create(record);
+  const { data: bridgeInfo } = await axios({
+    method: "post",
+    url: "https://nft-card.w3w.app/api/bridge",
+    data: record,
+  });
+
+  return bridgeInfo;
+  // Object.keys(record).forEach(
+  //   (value) => (value = BigNumber.from(value).toString())
+  // );
+  // const record = {
+  //   commitment: commitment,
+  //   token: "tokenabc",
+  //   fee: "1000",
+  //   value: "1000",
+  //   relayer: "relayer",
+  // };
+  // console.log({ BridgeInfo });
+  // const data = await BridgeInfo.create(record);
+  // console.log({ data });
+  // const data = await axios({
+  //   method: "get",
+  //   url: "https://jsonplaceholder.typicode.com/comments",
+  // });
+  // console.log(data);
+  // return data;
+};
+
+export const getBridgeInfos = async () => {
+  const { data: bridgeInfoList } = await axios({
+    method: "get",
+    url: "https://nft-card.w3w.app/api/bridge",
+  });
+  console.log(bridgeInfoList);
+  return bridgeInfoList;
 };
